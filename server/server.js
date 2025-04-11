7a0bd17dabf6b98e0caf4093e35b50b82b450acf
Aryan <72085890+Aryan-Sheregar@users.noreply.github.com>
2025-04-12 02:11:15 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -42,5 +42,5 @@ app.get("*", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
